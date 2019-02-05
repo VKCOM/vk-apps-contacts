202b64eca73398aab7373776e1d44bd4401fb3ce
Anton Tsivarev <a.tsivarev@corp.vk.com>
2019-02-05 12:15:38 +0300

    Updates

---
+++ precache-manifest.e426675a1104af5216dc50d36ce4c769.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "85edb7740176a87e81af",
+    "url": "/vk-apps-contacts/static/js/runtime~main.85edb774.js"
+  },
+  {
+    "revision": "94da23295b1041c1944b",
+    "url": "/vk-apps-contacts/static/js/main.94da2329.chunk.js"
+  },
+  {
+    "revision": "d3879bcc07a1a5edeec8",
+    "url": "/vk-apps-contacts/static/js/1.d3879bcc.chunk.js"
+  },
+  {
+    "revision": "d3879bcc07a1a5edeec8",
+    "url": "/vk-apps-contacts/static/css/1.c5760d2b.chunk.css"
+  },
+  {
+    "revision": "1c9bb2ecf2bd38494efc956f8c9bd0ce",
+    "url": "/vk-apps-contacts/index.html"
+  }
+];
